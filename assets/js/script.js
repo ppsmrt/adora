f64cc0a1648f7186ae98b989047a8e66c8cda8f4
Palani <124156458+ppsmrt@users.noreply.github.com>
2025-09-10 21:13:46 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,10 +27,12 @@ function showAd() {
   if (ad.type === "image") {
     adImage.src = ad.url;
     adImage.style.display = "block";
+    runImageAnimation(adImage);
     setTimeout(nextAd, 5000);
   } else if (ad.type === "video") {
     adVideo.src = ad.url;
     adVideo.style.display = "block";
+    adVideo.style.opacity = 1;
     adVideo.onended = nextAd;
   }
 }
@@ -38,4 +40,25 @@ function showAd() {
 function nextAd() {
   index = (index + 1) % ads.length;
   showAd();
+}
+
+/* Random animations for images */
+function runImageAnimation(el) {
+  const animations = [
+    "fadeIn",
+    "zoomIn",
+    "slideLeft",
+    "slideRight",
+    "rotateIn"
+  ];
+  const randomAnim = animations[Math.floor(Math.random() * animations.length)];
+  
+  el.className = ""; // reset
+  el.style.opacity = 1;
+  el.classList.add(randomAnim);
+
+  // Remove animation after it ends
+  setTimeout(() => {
+    el.classList.remove(randomAnim);
+  }, 2000);
 }
